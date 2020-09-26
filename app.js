060099c6f20f105e7d5054f9fa092ffd694f212d
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-09-26 14:44:26 -0700

    update color on calendar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   // Set dynamic date in header
+  let currentHourMoment = parseInt(moment().format("HH"));
   var currentDayElement = $("#currentDay");
 
   let updateTime = function () {
@@ -13,37 +14,62 @@ $(document).ready(function () {
   var AM = true;
   var militaryTimeArray = [];
   //Populate time blocks
-  for (let i = 1; i < 9; i++) {
-    var currentRow = $("<div class='row'>");
-    var currentHour = $("<div class='col-1 hour time-block'>");
-    if (AM) {
-      currentHour.text(displayTime + "AM");
-      militaryTimeArray.push(displayTime);
-    } else {
-      currentHour.text(displayTime + "PM");
-      militaryTimeArray.push(displayTime + 12);
-    }
-    var currentDescription = $("<input class='col-10 past description'>");
-    var currentSave = $("<div class='col-1 saveBtn'>");
-    $(".container").append(currentRow);
-    currentRow.append(currentHour);
-    currentRow.append(currentDescription);
-    currentRow.append(currentSave);
-    displayTime++;
-    if (displayTime > 12) {
-      displayTime = 1;
-      AM = false;
+  function displaySchedule() {
+    for (let i = 1; i < 9; i++) {
+      var militaryTime = displayTime;
+      var currentRow = $("<div class='row'>");
+      var currentHour = $("<div class='col-1 hour time-block'>");
+      if (AM) {
+        currentHour.text(displayTime + "AM");
+        // militaryTimeArray.push(displayTime);
+      } else {
+        currentHour.text(displayTime + "PM");
+        // militaryTimeArray.push(displayTime + 12);
+      }
+      var currentDescription = $("<input class='col-10 description'>");
+      if (
+        (displayTime < currentHourMoment && AM == true) ||
+        (displayTime + 12 < currentHourMoment && AM == false)
+      ) {
+        currentDescription.addClass("past");
+      } else if (
+        (displayTime > currentHourMoment && AM == true) ||
+        (displayTime + 12 > currentHourMoment && AM == false)
+      ) {
+        currentDescription.addClass("future");
+      } else {
+        currentDescription.addClass("present");
+      }
+      var currentSave = $("<div class='col-1 saveBtn'>");
+      $(".container").append(currentRow);
+      currentRow.append(currentHour);
+      currentRow.append(currentDescription);
+      currentRow.append(currentSave);
+      displayTime++;
+      if (displayTime > 12) {
+        displayTime = 1;
+        AM = false;
+      }
     }
   }
+  displaySchedule();
 
+  // function getData() {
+  //   var time = $(this).attr("data-number");
+  //   console.log(time);
+  // }
+  // getData();
   //Color code
   //1. convert display time to military time
-  console.log(militaryTimeArray);
+  // console.log(militaryTimeArray);
 
   // //Update colors when time past
-  let updateCalendarColor = function () {
-    let currentTime = parseInt(moment().format("HH"));
-  };
+
   updateCalendarColor();
   setInterval(updateCalendarColor, 1000);
+
+  $(".saveBtn").on("click");
+  //   //Store data
+  //   $("avebtton");onabort("clikd", function)
+  //   this.value
 });
