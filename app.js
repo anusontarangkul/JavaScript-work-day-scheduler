86b9c3889ad9b09637b5f4af8524c4ff45554579
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-09-26 10:50:54 -0700

    add hour to schedule

--- app.js
+++ app.js
@@ -9,7 +9,32 @@ $(document).ready(function () {
   updateTime();
   setInterval(updateTime, 1000);
 
-  //1 hour block calendar
+  var displayTime = 9;
+  var AM = true;
+  //Populate time blocks
+  for (let i = 1; i < 9; i++) {
+    var currentRow = $("<div class='row'>");
+    var currentHour = $("<div class='col-1 hour time-block'>");
+    if (AM) {
+      currentHour.text(displayTime + "AM");
+    } else {
+      currentHour.text(displayTime + "PM");
+    }
+    var currentDescription = $("<input class='col-10 past description'>");
+    var currentSave = $("<div class='col-1 saveBtn'>");
+    $(".container").append(currentRow);
+    currentRow.append(currentHour);
+    currentRow.append(currentDescription);
+    currentRow.append(currentSave);
+    displayTime++;
+    if (displayTime > 12) {
+      displayTime = 1;
+      AM = false;
+    }
+  }
+
+  //Add hour to left row
+  for (let i = 1; i < 9; i++) {}
 
   // //Update colors when time past
   let updateCalendarColor = function () {
