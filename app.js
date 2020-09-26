0d96c7150ad527004fe30a99021f00ae1105d2bb
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-09-25 22:13:04 -0700

    add dyanmic current date

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-/* Header
+var currentDateElement = $("#current-date");
+var currentDateMoment = moment().format("MMM Do YYYY");
+
+var currentDate = currentDateElement.text(currentDateMoment);
+currentDate.addClass("head-p");
